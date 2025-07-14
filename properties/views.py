b5b1cb13b7d559391b95a244bfd4b49ff4f798e2
TheKingsident <kingsident@gmail.com>
2025-07-14 14:04:15 +0100

    Refactor property_list view to return JSON response with property data and caching

--- properties/views.py
+++ properties/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from django.views.decorators.cache import cache_page
+from django.http import JsonResponse
 from .models import Property
 
 @cache_page(60 * 15)  # Cache for 15 minutes
 def property_list(request):
     properties = Property.objects.all()
-    return render(request, 'properties/property_list.html', {'properties': properties})
+    
+    # Convert properties to list of dictionaries
+    properties_data = []
+    for property in properties:
+        properties_data.append({
+            'id': property.id,
+            'title': property.title,
+            'description': property.description,
+            'price': str(property.price),
+            'location': property.location,
+            'created_at': property.created_at.isoformat(),
+        })
+    return JsonResponse({
+        'properties': properties_data,
+        'count': len(properties_data),
+        'cached': True,
+    })
